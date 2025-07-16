a8988e1d3156c0cfbe8e6c9d1432a3f8f8300666
Fuzail Kazi <mohdfuzailkazi14@gmail.com>
2025-07-16 18:18:48 +0530

    add trip function added

---
+++ app/trips/new/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { createTrip } from "@/lib/create-trip";
+import { cn } from "@/lib/utils";
+import { useTransition } from "react";
+
+export default function NewTrip() {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <div className="max-w-lg mx-auto mt-10">
+      <Card>
+        <CardHeader> New Trip</CardHeader>
+        <CardContent>
+          <form
+            action={(formData: FormData) => {
+              startTransition(() => {
+                createTrip(formData);
+              });
+            }}
+            className="space-y-6"
+          >
+            <div>
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+                placeholder="Japan trip"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Description
+              </label>
+              <textarea
+                name="description"
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+                placeholder="Japan trip was..."
+                required
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label
+                  htmlFor=""
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Start Date
+                </label>
+                <input
+                  type="date"
+                  name="startDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor=""
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  End Date
+                </label>
+                <input
+                  type="date"
+                  name="endDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                  required
+                />
+              </div>
+            </div>
+
+            <Button type="submit" disabled={isPending} className="w-full">
+              {isPending ? "Creating..." : "Create Trip"}
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function TripsPage() {
   const session = await auth();
@@ -13,9 +14,11 @@ export default async function TripsPage() {
 
   return (
     <div className="space-y-6 container mx-auto px-4 py-8">
-      <div>
-        <h1>Dashboard</h1>
-        <Button>New Trip</Button>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold tracking-tight"> Dashboard</h1>
+        <Link href="/trips/new">
+          <Button>New Trip</Button>
+        </Link>
       </div>
     </div>
   );
